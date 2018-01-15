c88446d25ec6cf9ab9a7e997ebeee0c20ffaa363
Daniel Yim <danielyim@gmail.com>
2018-01-15 04:03:55 -0800

    adding content

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
-import './App.css'
+import Input from './Input'
+import styles from './App.scss'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={styles.wrapper}>
+        <section className={styles.container}>
+          <span className={styles.label}>That time in</span>
+          <Input type="text" />
+          <span className={styles.label}>where</span>
+          <Input type="text" />
+          <span className={styles.label}>.</span>
+        </section>
       </div>
     )
   }
---
+++ src/Input.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export default styled.input`
+  background-color: unset;
+  border: none;
+  border-bottom: 1px solid black;
+  outline: 0;
+  margin: 0 .7rem;
+  padding-bottom: 0.5rem;
+  font-size: 1rem;
+`
